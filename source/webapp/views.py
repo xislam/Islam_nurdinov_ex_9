80243da05027b742e4f4a51eb2f2851d2c1cc7b4
xislam <nurdinovislam4@gmail.com>
2020-10-24 12:00:00 +0600

    я перехожу к шаблонам CRUD потом я должен продолжить писать views

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView, DetailView
+
+from webapp.models import PhotoGallery
 
 
-class IndexView():
-    pass
+class IndexView(ListView):
+    template_name = ''
+    context_object_name = 'photos'
+    model = PhotoGallery
+    ordering = '-creation_date'
 
 
-class IndexView():
-    pass
+class PhotoGalleryView(DetailView):
+    template_name = ''
+    pk_url_kwarg = 'pk'
+    model = PhotoGallery
 
 
 class IndexView():

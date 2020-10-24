2c5311d6e79897fc51df496139b18a4e631caaac
xislam <nurdinovislam4@gmail.com>
2020-10-24 11:44:42 +0600

    В вебап в файле models.py создал таблицу PhotoGallery , это превый этам

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class PhotoGallery(models.Model):
+    img = models.ImageField(null=False, blank=False, upload_to='media', verbose_name='Фото')
+    description = models.TextField(null=False, blank=False, max_length=1000, verbose_name='Подпись')
+    author_name = models.CharField(null=False, blank=False, max_length=60, verbose_name='Автор')
+    creation_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return self.description
+
+    class Meta:
+        verbose_name = 'Фото'
+        verbose_name_plural = 'Фотографии'
+
+

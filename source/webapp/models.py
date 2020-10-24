01cef1b2dc8d25e4b9bd08b335d966717ae2b5bb
xislam <nurdinovislam4@gmail.com>
2020-10-24 13:40:09 +0600

    проверка на пуш

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class PhotoGallery(models.Model):
+    user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE, verbose_name='Пользователь')
     img = models.ImageField(null=False, blank=False, upload_to='media', verbose_name='Фото')
     description = models.TextField(null=False, blank=False, max_length=1000, verbose_name='Подпись')
     creation_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
